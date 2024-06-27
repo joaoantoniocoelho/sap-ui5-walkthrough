a9195a1defe17e346ee8c66a6954733906f6bcf8
João Coelho <joaoantonioscoelho@gmail.com>
2024-06-27 11:16:11 -0300

    step 5 - controllers

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    return Controller.extend("ui5.walkthrough.controller.App", {
+        onShowHello() {
+            
+            alert('Hello Controller!')
+        }
+    })
+})
