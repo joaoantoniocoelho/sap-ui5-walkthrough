333c6ba47df7d71dee7164235bce6fe4f5f5a590
João Coelho <joaoantonioscoelho@gmail.com>
2024-06-27 14:13:26 -0300

    step 9 - component configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,23 +5,6 @@ sap.ui.define([
     "sap/ui/model/resource/ResourceModel"
 ], (Controller, MessageToast, JSONModel, ResourceModel) => {
     return Controller.extend("ui5.walkthrough.controller.App", {
-        onInit() {
-            const oData = {
-                recipient: {
-                    name: "World"
-                }
-            }
-
-            const oModel = new JSONModel(oData)
-            this.getView().setModel(oModel)
-
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-            })
-
-            this.getView().setModel(i18nModel, "i18n")
-        },
-
         onShowHello() {
             // read the message from i18n model
             const oBundle = this.getView().getModel("i18n").getResourceBundle()
