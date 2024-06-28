28d037245c94849fc191d8b6cfad7a5672f1e442
João Coelho <joaoantonioscoelho@gmail.com>
2024-06-28 14:28:49 -0300

    step 23 - filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,17 +1,42 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"../model/formatter"
-], (Controller, JSONModel, formatter) => {
-	"use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  (Controller, JSONModel, formatter, Filter, FilterOperator) => {
+    "use strict";
 
-	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
-		formatter: formatter,
-		onInit() {
-			const oViewModel = new JSONModel({
-				currency: "EUR"
-			});
-			this.getView().setModel(oViewModel, "view");
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+      formatter: formatter,
+
+      onInit() {
+        const oViewModel = new JSONModel({
+          currency: "EUR",
+        });
+        this.getView().setModel(oViewModel, "view");
+      },
+
+	  onFilterInvoices(oEvent) {
+		// build filter
+		const aFilter = []
+		const sQuery = oEvent.getParameter("query")
+
+		if (sQuery) {
+			aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery))
 		}
-	});
-});
+
+		// get the list
+		const oList = this.byId("invoiceList")
+
+		// get list items
+		const oBinding = oList.getBinding("items");
+
+		// apply filter
+		oBinding.filter(aFilter)
+	  }
+    });
+  }
+);
